f15faef5e3f59aae56f688d36e5cdc256dbec76b
Miguel2493 <a2493m@gmail.com>
2021-08-03 19:53:03 -0500

    fetch Request for username

--- src/index.js
+++ src/index.js
@@ -1,14 +1,35 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { useEffect, useState } from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+
+fetch(
+  "https://strangers-things.herokuapp.com/api/2104-UIC-RM-WEB-PT/users/register",
+  {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      user: {
+        username: "spongebob",
+        password: "KrabbyPatty",
+      },
+    }),
+  }
+)
+  .then((response) => response.json())
+  .then((result) => {
+    console.log(result);
+  })
+  .catch(console.error);
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
